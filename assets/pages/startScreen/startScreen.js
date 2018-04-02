870f266ecb0114795ababaa85382a6507f6bb450
Owen Mundy <omundy@gmail.com>
2018-04-02 15:45:08 -0400

    Add dummy link to register

--- assets/pages/startScreen/startScreen.js
+++ assets/pages/startScreen/startScreen.js
@@ -1,6 +1,20 @@
 "use strict";
 
+let tally_meta = getMeta();
+
+function getMeta() {
+	chrome.runtime.sendMessage({
+		'action': 'getMeta'
+	}, function(response) {
+		//console.log("getMeta()",JSON.stringify(response.data));
+		tally_meta = response.data;
+	});
+}
 
 $(document).on('click','#launchRegistrationBtn',function(){
-    launchRegistrationPage();
+    chrome.tabs.create({
+        url: tally_meta.website + "/signup"
+    }, function(tab) {
+        console.log(">>>>> launching registration page", tab.url);
+    });
 });
