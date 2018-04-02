93884e0ac27adf9e4ec7996020d89d9343b71b4c
Owen Mundy <omundy@gmail.com>
2018-04-02 10:16:30 -0400

    Adds date functions

--- assets/js/functions/fs-dates.js
+++ assets/js/functions/fs-dates.js
@@ -1,12 +1,12 @@
 "use strict";
 
 /*  DATE FUNCTIONS
-******************************************************************************/
+ ******************************************************************************/
 
 /**
  *	Return a date in ISO string
  */
-function returnDateISO(_date=null){
+function returnDateISO(_date = null) {
 	var date = new Date();
 	if (_date != null)
 		date = _date;
@@ -15,4 +15,38 @@ function returnDateISO(_date=null){
 	//Removing milisseconds;
 	s = s.substring(0, s.indexOf('.'));
 	return s;
+}
+
+/**
+ *	Check if a date string is valid
+ */
+function isValidDateString(str) {
+	let timestamp = Date.parse(str);
+	//console.log("timestamp", timestamp);
+	if (isNaN(timestamp) == false) {
+		//var d = new Date(timestamp);
+		return true;
+	} else {
+		return false;
+	}
+}
+
+
+/**
+ *	Format a date and return number millis between now and date
+ *	Return
+ *		false = date is invalid
+ *		positive number = date is in the future
+ *		negative number = date is in the past
+ *		0 = date is now
+ */
+function returnDateDifferenceMinutes(str) {
+	if (!isValidDateString(str)) return false;
+	let now = new Date(),
+		then = new Date(str),
+		difference = then.getTime() - now.getTime();
+	// console.log("now", now.getTime());
+	// console.log("then", then.getTime());
+	// console.log("difference", difference);
+	return difference;
 }
