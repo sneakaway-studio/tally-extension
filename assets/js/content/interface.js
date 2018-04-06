34161c140f9ab9e8ab6a6e0932ee0e406cd2b391
Owen Mundy <omundy@gmail.com>
2018-04-05 21:28:06 -0400

    Adds no-print class to everything

--- assets/js/content/interface.js
+++ assets/js/content/interface.js
@@ -3,7 +3,7 @@
 $( function() {
 
     function addBaseHTML(){
-        let str =   "<div id='tally_wrapper' class='reset-this-root reset-this'>"+
+        let str =   "<div id='tally_wrapper' class='reset-this-root reset-this no-print'>"+
                         "<div id='tally_click_visual'></div>"+
                             "<div class='tally_monster_sprite'></div>"+
                         "<div id='tally'></div>"+
