0f4a0db7a67fe14c9bb46066f496ac4d58f99506
Owen Mundy <omundy@gmail.com>
2018-04-05 22:28:38 -0400

    Clean up debug

--- assets/js/content/debug.js
+++ assets/js/content/debug.js
@@ -2,45 +2,47 @@
 
 
 /*  DEBUGGER
-******************************************************************************/
+ ******************************************************************************/
 
 var Debug = (function() {
 	// PRIVATE
 
 	// add the debugger
-	function add(){
+	function add() {
 		if (!prop(tally_options) || !tally_options.showDebugger) return;
 
-		let str = "<div id='tyd' class='draggable data-field grabbable'>"+"</div>";
+		let str = "<div id='tyd' class='draggable data-field grabbable'>" + "</div>";
 		$('#tally').append(str);
 		// make it draggable
 		$("#tyd").draggable({
 			axis: "y",
-			drag: function(){
+			drag: function() {
 				//console.log("draggable:drag");
 				// var offset = $(this).offset();
 				// var xPos = offset.left;
 				// var yPos = offset.top - $(window).scrollTop();
 				// tally_options.debuggerPosition = [xPos,yPos];
 			},
-			stop: function(){
+			stop: function() {
 				//console.log("draggable:stop");
 				//saveOptions("tyd.drag.stop");
 			}
 		});
 	}
-	function update(){
+
+	function update() {
 		if (!prop(tally_options) || !tally_options.showDebugger) return;
 		if (!$("#tyd").length) return;
 
-		var str =   "<div>"+
-					"<b>tally_user</b>: " + JSON.stringify(tally_user) +"<br>"+
-					//"tally_options: " + JSON.stringify(tally_options) +"<br>"+
-					//"<b>pageData</b>: " + JSON.stringify(pageData) +"<br>"+
-					"<b>pageData.tags ("+ pageData.tags.length +")</b>: " + JSON.stringify(pageData.tags) +"<br>"+
-					"<b>pageData.trackers ("+ pageData.trackers.length +")</b>: " + JSON.stringify(pageData.trackers) +"<br>"+
-					"<b>tally_recent_monsters ("+ objLength(tally_recent_monsters) +")</b>: " + JSON.stringify(tally_recent_monsters) +"<br>"+
-					"</div>";
+		var str = "<div>" +
+			"<b>tally_user.score</b>: " + JSON.stringify(tally_user.score) + "<br>" +
+			"<b>tally_user.achievements</b>: " + JSON.stringify(tally_user.achievements) + "<br>" +
+			//"tally_options: " + JSON.stringify(tally_options) +"<br>"+
+			//"<b>pageData</b>: " + JSON.stringify(pageData) +"<br>"+
+			"<b>pageData.tags (" + pageData.tags.length + ")</b>: " + JSON.stringify(pageData.tags) + "<br>" +
+			"<b>pageData.trackers (" + pageData.trackers.length + ")</b>: " + JSON.stringify(pageData.trackers) + "<br>" +
+			"<b>tally_recent_monsters (" + objLength(tally_recent_monsters) + ")</b>: " + JSON.stringify(tally_recent_monsters) + "<br>" +
+			"</div>";
 		$('#tyd').html(str);
 	}
 
