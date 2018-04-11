79cb570d85f7ca6583cb318f4a038ef81c756901
Owen Mundy <omundy@gmail.com>
2018-04-11 12:48:20 -0500

    Changes to html in interface.js

--- assets/js/content/interface.js
+++ assets/js/content/interface.js
@@ -5,10 +5,21 @@ $( function() {
     function addBaseHTML(){
         let str =   "<div id='tally_wrapper' class='reset-this-root reset-this no-print'>"+
                         "<div id='tally_click_visual'></div>"+
-                        "<div class='tally_monster_sprite'></div>"+
-                        "<div class='tally_award_monster'></div>"+
-                        "<div class='tally_award_text'></div>"+
+                        "<div id='tally_monster_sprite'></div>"+
+
+                        "<div class='tally_award_monster_wrapper'>"+
+                            "<div class='tally_award_monster'></div>"+
+                        "</div>"+
+                        "<div class='tally_award_text_wrapper'>"+
+                            "<div class='tally_award_text'>"+
+                                "<div class='award_title'></div>" +
+                    			"<div class='award_subtitle'></div>" +
+                    			"<div class='award_fact_title'></div>" +
+                    			"<div class='award_fact'></div>" +
+                            "</div>"+
+                        "</div>"+
                         "<div class='tally_award_background'></div>"+
+
                         "<div id='tally'></div>"+
                     "</div>";
         $('body').append(str);
