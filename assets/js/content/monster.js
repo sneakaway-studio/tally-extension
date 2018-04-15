cb181700d604e1b66053b8eadd6d17ef7cdbe442
Ryan Strauss <ryanrstrauss@icloud.com>
2018-04-15 14:22:20 -0400

    Resolve conflicts

--- assets/js/content/monster.js
+++ assets/js/content/monster.js
@@ -1,232 +1,5 @@
 "use strict";
 
-<<<<<<< HEAD
-var Monster = (function () {
-
-    let MONSTER_DEBUG = true,
-        current = "",
-        secondsBeforeDelete = 60; // 60 seconds for testing
-
-    /**
-     *    Test to make sure API is working
-     */
-    function test() {
-        // make sure there are monsters nearby
-        if (!tally_recent_monsters || objLength(tally_recent_monsters) <= 0) return;
-
-        // TESTING
-        let _mid = randomObjKey(tally_recent_monsters),
-            _stage = 3;
-        tally_recent_monsters[_mid] = create(_mid, _stage);
-        tally_recent_monsters[_mid].captured = 1;
-        if (MONSTER_DEBUG) console.log("+++++ Monster.test()", MonsterData.dataById[_mid]);
-        // save
-        saveRecentMonsters();
-        // set the skin color
-        Skin.setStage(tally_recent_monsters[_mid].stage);
-        current = _mid;
-        Thought.showThought(Thought.getThought(["monster", "launch", 0]), true);
-        launch(_mid);
-        //capture(_mid);
-    }
-
-
-    /**
-     *    Initial check function, refreshes recent monsters from back end continues to next
-     */
-    function check() {
-        // don't check if disabled
-        if (tally_options.gameMode === "disabled") return;
-        checkRecentTimes();
-    }
-
-    /**
-     *    Make sure all monsters are recent, deletes those that aren't
-     */
-    function checkRecentTimes() {
-        let now = Date.now(),
-            highestStage = 0;
-        // make sure tally_recent_monsters exists
-        if (tally_recent_monsters && objLength(tally_recent_monsters) > 0) {
-            // loop through them
-            for (var mid in tally_recent_monsters) {
-                // how long has it been since this monster was seen?
-                // if longer than 5 mins (300 secs) then delete
-                let seconds = ((now - tally_recent_monsters[mid].updatedAt) / 1000);
-                if ((seconds) > secondsBeforeDelete) {
-                    if (MONSTER_DEBUG) console.log("..... checkRecentTimes() -> DELETING", MonsterData.dataById[mid].slug, "seconds", seconds);
-                    delete tally_recent_monsters[mid];
-                }
-                // skin should reflect highest stage
-                if (prop(tally_recent_monsters[mid]) && tally_recent_monsters[mid].stage > highestStage)
-                    highestStage = tally_recent_monsters[mid].stage;
-            }
-        }
-        saveRecentMonsters();
-        // set the skin color
-        Skin.setStage(highestStage);
-        // continue
-        checkForTagMatches();
-    }
-
-    /**
-     *    Check the page for a monster
-     */
-    function checkForTagMatches() {
-        //console.log('>>>>> Monster.check()', pageData.tags);
-        // loop through the tags on the page
-        for (var i = 0, l = pageData.tags.length; i < l; i++) {
-            // save reference
-            let tag = pageData.tags[i];
-            // if tag is in list
-            if (MonsterData.idsByTag[tag]) {
-                // save reference
-                let arr = MonsterData.idsByTag[tag];
-                // the monster id that will be picked
-                let mid = 0;
-                // if there are matches...
-                if (arr.length > 1) {
-                    // pick random monster id from list, this will be the page monster
-                    mid = arr[Math.floor(Math.random() * arr.length)];
-                    if (MONSTER_DEBUG) console.log('!!!!! #' + tag, "has", arr.length, 'MATCHES', arr, "randomly selecting...", MonsterData.dataById[mid].slug);
-                    // we have identified a match, let's handle the monster
-                    handleMatch(mid);
-                    break;
-                }
-            }
-        }
-    }
-
-
-    /**
-     *    Create a monster
-     */
-    function create(_mid, _stage = 1) {
-        if (!prop(_mid) || !prop(_stage) || !prop(MonsterData.dataById[_mid])) return;
-        if (MONSTER_DEBUG) console.log('..... Monster.create()', _mid, _stage, MonsterData.dataById[_mid]);
-        let monster = {
-            "captured": 0,
-            "level": 1,
-            "mid": _mid,
-            "stage": _stage,
-            "slug": MonsterData.dataById[_mid].slug,
-            "updatedAt": Date.now()
-        };
-        return monster;
-    }
-
-    /**
-     *    A monster has been matched to page tags, handle it
-     */
-    function handleMatch(mid) {
-        let launchMonster = false;
-
-        // if the monster id does not exist in recent
-        if (!prop(tally_recent_monsters[mid])) {
-            // add it
-            tally_recent_monsters[mid] = create(mid);
-        }
-        // otherwise monster has been seen before
-        else {
-            // randomizer
-            let r = Math.random();
-            // what stage are we at with this monster?
-            if (tally_recent_monsters[mid].stage == 0) {
-                // do nothing
-            } else if (tally_recent_monsters[mid].stage == 1) {
-                if (r < 0.2) {
-                    // go back to normal stage
-                    tally_recent_monsters[mid].stage == 0
-                } else if (r < 0.4) {
-                    // do nothing
-                } else if (r < 0.7) {
-                    // show them a thought but don't change stage
-                    Thought.showThought(Thought.getThought(["monster", "far", 0]), true);
-                } else {
-                    // or prompt stage 2
-                    tally_recent_monsters[mid].stage = 2;
-                    Thought.showThought(Thought.getThought(["monster", "close", 0]), true);
-                }
-            } else if (tally_recent_monsters[mid].stage == 2) {
-                if (r < 0.4) {
-                    // do nothing
-                } else if (r < 0.7) {
-                    // show them a thought
-                    Thought.showThought(Thought.getThought(["monster", "close", 0]), true);
-                } else {
-                    // or prompt stage 3 - launch
-                    tally_recent_monsters[mid].stage = 3;
-                    launchMonster = true;
-                }
-            }
-
-            //if (MONSTER_DEBUG) console.log('!!!!! handleMatch()', MonsterData.dataById[mid].slug, tally_recent_monsters[mid]);
-        }
-        if (MONSTER_DEBUG) console.log('!!!!! Monster.handleMatch()', MonsterData.dataById[mid].slug, "stage =", tally_recent_monsters[mid].stage);
-        // set skin
-        Skin.setStage(tally_recent_monsters[mid].stage);
-        // save monsters
-        saveRecentMonsters();
-        // should we launch a monster?
-        if (launchMonster) {
-            current = mid;
-            Thought.showThought(Thought.getThought(["monster", "launch", 0]), true);
-            launch(mid);
-        }
-    }
-
-    /**
-     *    Launch a product monster
-     */
-    function launch(mid) {
-        if (MONSTER_DEBUG) console.log('!!!!! Monster.launch()', mid, tally_recent_monsters[mid].stage);
-        // don't launch them if game isn't running in full
-        if (tally_options.gameMode != "full") return;
-
-        let monster = MonsterData.dataById[mid],
-            level = 1;
-
-        // if they already have this one, increase level
-        if (tally_user.monsters[mid])
-            level = tally_user.monsters[mid].level + 1;
-        // reference to image file
-        var url = chrome.extension.getURL('assets/img/monsters/' + monster.mid + '-anim-sheet.png');
-        // set content
-        $('.tally_monster_sprite_inner').css('background-image', 'url( ' + url + ')');
-
-        let pos = "bottom";
-        launchFrom(mid, pos, level);
-
-        // temp
-        $.growl({
-            title: "LAUNCHING MONSTER!!!",
-            message: "MONSTER: " + monster.name + " [" + monster.mid + "] <br>STAGE: " + tally_recent_monsters[mid].stage
-        });
-
-        // somewhere here we would attach a click listener to the monster
-        // let's assume we've done that so we can test capture()
-        //capture(mid, level);
-
-        // temp: call after capture OR miss
-        setTimeout(function () {
-            reset(mid);
-        }, 2000);
-
-    }
-
-    function launchFrom(_mid, _pos, _level) {
-        console.log("launchFrom()", _mid, _pos)
-        let _duration = 4500;
-
-        let pathID = randomObjKey(MonsterPaths);
-
-        let x = MonsterPaths[pathID].x,
-            y = MonsterPaths[pathID].y,
-            scale = MonsterPaths[pathID]['scale'];
-
-        // position monster
-        $('.tally_monster_sprite_container').css({
-=======
 var Monster = (function() {
 
 	let MONSTER_DEBUG = true,
@@ -450,7 +223,7 @@ var Monster = (function() {
 
 		// position monster
 		$('.tally_monster_sprite_container').css({
->>>>>>> 0af26519c1070bd9f9fe554d388e1f3c07ccc8a3
+
             'top': y - 250,
             'left': x - 200,
             'display': 'block',
@@ -479,7 +252,7 @@ var Monster = (function() {
             loop: true
         });
 
-<<<<<<< HEAD
+
         $(document).on('click', '.tally_monster_sprite', function () {
             showAward(_mid);
             capture(_mid, _level);
@@ -591,7 +364,7 @@ var Monster = (function() {
         current: getCurrent,
         test: test
     };
-=======
+
 		$(document).on('click', '.tally_monster_sprite', function() {
 			showAward(_mid);
 			//capture(_mid, _level);
@@ -706,5 +479,5 @@ var Monster = (function() {
 		current: getCurrent,
 		test: test
 	};
->>>>>>> 0af26519c1070bd9f9fe554d388e1f3c07ccc8a3
+
 }());
