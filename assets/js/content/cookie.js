abce25c02fedd401a2b3de209917e89ac8f7dff8
Owen Mundy <omundy@gmail.com>
2018-11-15 15:16:24 -0500

    Adds cookie

---
+++ assets/js/content/cookie.js
@@ -0,0 +1,32 @@
+"use strict";
+
+/*  COOKIE
+ ******************************************************************************/
+
+var Cookie = (function() {
+	// PRIVATE
+
+
+	// name
+		// health cookie
+		// manna cookie
+
+	// _duration once/or time limit
+	// value = e.g. 200
+	// category
+		// health
+		// str
+		// df
+		// attacl
+
+
+//	pickup()
+//  generate()
+//  use()
+
+
+	// PUBLIC
+	return {
+
+	};
+})();
