42ba2459abe1af96892cd57bc8a3dd40d5d89618
Owen Mundy <omundy@gmail.com>
2019-04-11 21:29:27 -0400

    Change to self and opp in line with other functions in battle

--- assets/js/content/battle-math.js
+++ assets/js/content/battle-math.js
@@ -8,60 +8,52 @@ var BattleMath = (function() {
 
 
 
-	function updateHealth(attackObj,/*characterObj*/,/*otherCharaterObj*/) {
-		// get the latest stats
-		var enemStats = otherCharacterObj.stats();
-		var charStats = characterObj.stats();
+	function updateHealth(attackObj, selfStats, oppStats) {
 		// do stuff
 
 		// if you pass a stats object it will update
 		Tally.stats(tallyStats);
 	}
-	function updateAttack(attackObj,/*characterObj*/,/*otherCharaterObj*/) {
-		var enemStats = otherCharacterObj.stats();
-		var charStats = characterObj.stats();
-		if(attackObj.selfAtk != NULL){
+
+	function updateAttack(attackObj, selfStats, oppStats) {
+		if (attackObj.selfAtk != NULL) {
 			charStats.Attack = charStats.Attack * attackObj.selfAtk;
 		}
-		if(attackObj.oppAtk != NULL){
-			enemStats.Attack = enemStats.Attack * attackObj.selfAtk;
+		if (attackObj.oppAtk != NULL) {
+			selfStats.Attack = selfStats.Attack * attackObj.selfAtk;
 		}
 	}
-	function updateStamina(attackObj,/*characterObj*/,/*otherCharacterObj*/) {
-		var enemStats = otherCharacterObj.stats();
-		var charStats = characterObj.stats();
-		if(attackObj.staminaCost != NULL){
+
+	function updateStamina(attackObj, selfStats, oppStats) {
+		if (attackObj.staminaCost != NULL) {
 			charStats.Stamina = charStats.Stamina - attackObj.staminaCost;
 		}
 	}
-	function updateAccuracy(attackObj,/*characterObj*/,/*otherCharacterObj*/) {
-		var enemStats = otherCharacterObj.stats();
-		var charStats = characterObj.stats();
-		if(attackObj.selfAcc != NULL){
+
+	function updateAccuracy(attackObj, selfStats, oppStats) {
+		if (attackObj.selfAcc != NULL) {
 			charStats.Accuracy = charStats.Accuracy * attackObj.selfAcc;
 		}
-		if(attackObj.oppAcc != NULL){
-			enemStats.Accuracy = enemStats.Accuracy * attackObj.oppAcc;
+		if (attackObj.oppAcc != NULL) {
+			selfStats.Accuracy = selfStats.Accuracy * attackObj.oppAcc;
 		}
 	}
-	function updateEvasion(attackObj,/*characterObj*/,/*otherCharacterObj*/) {
-		var enemStats = otherCharacterObj.stats();
-		var charStats = characterObj.stats();
-		if(attackObj.selfEva != NULL){
+
+	function updateEvasion(attackObj, selfStats, oppStats) {
+		if (attackObj.selfEva != NULL) {
 			charStats.Evasion = charStats.Evasion * attackObj.selfEva;
 		}
-		if(attackObj.oppEva != NULL){
-			enemStats.Evasion = enemStats.Evasion * attackObj.oppEva;
+		if (attackObj.oppEva != NULL) {
+			selfStats.Evasion = selfStats.Evasion * attackObj.oppEva;
 		}
 	}
-	function updateDefense(attackObj,/*characterObj*/,/*otherCharacterObj*/) {
-		var enemStats = otherCharacterObj.stats();
-		var charStats = characterObj.stats();
-		if(attackObj.selfDef != NULL){
+
+	function updateDefense(attackObj, selfStats, oppStats) {
+		if (attackObj.selfDef != NULL) {
 			charStats.Defense = charStats.Defense * attackObj.selfDef;
 		}
-		if(attackObj.oppDef != NULL){
-			enemStats.Attack = enemStats.Attack * attackObj.selfDef;
+		if (attackObj.oppDef != NULL) {
+			selfStats.Attack = selfStats.Attack * attackObj.selfDef;
 		}
 	}
 
