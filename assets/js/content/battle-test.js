6f727ae0c54f4055615e685c1560dde366dd2418
Owen Mundy <omundy@gmail.com>
2019-05-02 14:27:32 -0400

    Adds BattleTest

---
+++ assets/js/content/battle-test.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/*  BATTLE TEST
+ ******************************************************************************/
+
+window.BattleTest = (function() {
+	// PRIVATE
+
+
+
+	function test1(){
+		// run test
+	}
+
+
+
+	// PUBLIC
+	return {
+		test1: test1,
+
+	};
+})();
