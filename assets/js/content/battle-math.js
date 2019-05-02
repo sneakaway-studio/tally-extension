f96c9930c3fc8dab6a494ff782d3f2bd9488d34d
DLammens <36461609+DLammens@users.noreply.github.com>
2019-05-02 14:11:51 -0400

    Update battle-math.js

--- assets/js/content/battle-math.js
+++ assets/js/content/battle-math.js
@@ -18,123 +18,71 @@ var BattleMath = (function() {
 		change.val = Math.ceil(Math.random()*10);
 		return change;
 	}
+	
+	function damageCalc(attackObj, selfStats, oppStats){
+		//Calculation
+	}
+	
+	function willHit(attackObj, selfStats, oppStats){
+		var hitChance = attackObj.accuracy * (selfStats.Accuracy/oppStats.Evasion);
+	}
 
 	function updateAllStats(attackObj, selfStats, oppStats){
-		//I think this will work better
-	}
-	function updateHealth(attackObj, selfStats, oppStats) {
-		// track changes
-		let change = {"val":0,"str":"health"};
-		// I don't understand this
+		let change = [{"val":0, "str":""}];
 		
-		// do math and store in val
-		selfStats.health = selfStats.health * (oppStats.str * attackObj.damage)
-		// then apply value to stats
-		
-		// save stats
-
-		// return what happened so we can log it
-		return change.val +" "+ change.str;
-	}
-
-	function updateAttack(attackObj, selfStats, oppStats) {
-		// track changes
-		let change = {"val":0,"str":"attack"};
-		tempRandomChangeVal(change); // Daniel, delete this once you get math working
-
+		//HEALTH: FROM DAMAGE
+		if (attackObj.damage != NULL) {
+			selfStats.Health = selfStats.Health * damageCalc(attackObj, selfStats, oppStats);
+		}
+		//ATTACK STAT
 		if (attackObj.selfAtk != NULL) {
 			selfStats.Attack = selfStats.Attack * attackObj.selfAtk;
 		}
 		if (attackObj.oppAtk != NULL) {
 			oppStats.Attack = oppStats.Attack * attackObj.oppAtk;
 		}
-
-		// return what happened so we can log it
-		return change.val +" "+ change.str;
-	}
-
-	function updateStamina(attackObj, selfStats, oppStats) {
-		// track changes
-		let change = {"val":0,"str":"stamina"};
-		tempRandomChangeVal(change); // Daniel, delete this once you get math working
-
+		//STAMINA COST
 		if (attackObj.staminaCost != NULL) {
 			selfStats.Stamina = selfStats.Stamina - attackObj.staminaCost;
 		}
-
-		// return what happened so we can log it
-		return change.val +" "+ change.str;
-	}
-
-	function updateAccuracy(attackObj, selfStats, oppStats) {
-		// track changes
-		let change = {"val":0,"str":"accuracy"};
-		tempRandomChangeVal(change); // Daniel, delete this once you get math working
-
+		//ACCURACY STAT
 		if (attackObj.selfAcc != NULL) {
 			selfStats.Accuracy = selfStats.Accuracy * attackObj.selfAcc;
 		}
 		if (attackObj.oppAcc != NULL) {
 			oppStats.Accuracy = oppStats.Accuracy * attackObj.oppAcc;
 		}
-
-		// return what happened so we can log it
-		return change.val +" "+ change.str;
-	}
-
-	function updateEvasion(attackObj, selfStats, oppStats) {
-		// track changes
-		let change = {"val":0,"str":"evasion"};
-		tempRandomChangeVal(change); // Daniel, delete this once you get math working
-
+		//EVASION STAT
 		if (attackObj.selfEva != NULL) {
-			charStats.Evasion = charStats.Evasion * attackObj.selfEva;
+			selfStats.Evasion = selfStats.Evasion * attackObj.selfEva;
 		}
 		if (attackObj.oppEva != NULL) {
-			selfStats.Evasion = selfStats.Evasion * attackObj.oppEva;
+			oppStats.Evasion = oppStats.Evasion * attackObj.oppEva;
 		}
-
-		// return what happened so we can log it
-		return change.val +" "+ change.str;
-	}
-
-	function updateDefense(attackObj, selfStats, oppStats) {
-		// track changes
-		let change = {"val":0,"str":"defense"};
-		tempRandomChangeVal(change); // Daniel, delete this once you get math working
-
+		//DEFENSE STAT
 		if (attackObj.selfDef != NULL) {
-			charStats.Defense = charStats.Defense * attackObj.selfDef;
+			selfStats.Defense = selfStats.Defense * attackObj.selfDef;
 		}
 		if (attackObj.oppDef != NULL) {
-			selfStats.Attack = selfStats.Attack * attackObj.selfDef;
+			oppStats.Attack = oppStats.Attack * attackObj.selfDef;
 		}
+		//HEAL
+		if (attackObj.heal != NULL) {
+			selfStats.Health = selfStats.Health * attackObj.heal;
+		}
+		
 
-		// return what happened so we can log it
-		return change.val +" "+ change.str;
 	}
 
 
 
 	// PUBLIC
 	return {
-		updateHealth: function(attackObj) {
-			return updateHealth(attackObj);
-		},
-		updateAttack: function(attackObj) {
-			return updateAttack(attackObj);
-		},
-		updateStamina: function(attackObj) {
-			return updateStamina(attackObj);
-		},
-		updateAccuracy: function(attackObj) {
-			return updateAccuracy(attackObj);
-		},
-		updateEvasion: function(attackObj) {
-			return updateEvasion(attackObj);
-		},
-		updateDefense: function(attackObj) {
-			return updateDefense(attackObj);
-		},
+		updateAllStats: function(attackObj, selfStats, oppStats) {
+			return updateAllStats(attackObj, selfStats, oppStats);
+		}
+		damageCalc: function(attackObj, selfStats, oppStats) {
+			return damageCalc(attackObj, selfStats, oppStats);
+		}
 	};
 })();
