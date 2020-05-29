2f4f0bec3d47726e49760d65e4bf14ae1fef0023
Owen Mundy <omundy@gmail.com>
2020-05-29 12:11:16 -0400

    Standardize Item data filenames

--- assets/js/content/disguise.js
+++ assets/js/content/disguise.js
@@ -15,7 +15,7 @@ window.Disguise = (function() {
  * 	Display a random disguise
  */
 	function randomizer(playDialogue = true) {
-		try {
+		try { return;
 			currentDisguiseObj = FS_Object.randomObjProperty(DisguiseData.data);
 			if (DEBUG) console.log("😎 Disguise.randomizer() [1] currentDisguiseObj =", currentDisguiseObj);
 			display(playDialogue);
@@ -78,10 +78,60 @@ window.Disguise = (function() {
 
 
 
+	/**
+	 *	Check to see if we should reward Tally with a new disguise
+	 */
+	function reward() {
+		try {
+			if (DEBUG) console.log("😎 Disguise.reward() [1]");
+
+			// if ()
+			//
+			//
+			//
+			// // get random attack
+			// let attack = AttackData.returnAttack(name, type);
+			//
+			// // make sure tally doesn't already have that attack
+			// let safety = 0;
+			// while (prop(tally_user.attacks[attack.name])) {
+			// 	// if so get a new one, passing name and type if set
+			// 	attack = AttackData.returnAttack(name, type);
+			// 	// exit if all attacks have been rewarded
+			// 	if (++safety > 10) break;
+			// }
+			// if (DEBUG) console.log("💥 BattleAttack.rewardAttack() name=" + attack.name + ", type=" + attack.type);
+			//
+			//
+			// // attack is selected by default
+			// attack.selected = 1;
+			// // unless # selected is already >= to limit
+			// let selected = returnAttacksSelected();
+			// if (selected >= 4) {
+			// 	attack.selected = 0;
+			// } else {
+			// 	Progress.update("attacksSelected", selected + 1);
+			// }
+			//
+			// // update progress
+			// Progress.update("disguisesAwarded", FS_Object.objLength(tally_user.attacks));
+			// // save in background (and on server)
+			// TallyData.handle("itemData", "attacks", attack, "💥 BattleAttack.rewardAttack()");
+			//
+			// // tell user
+			// Dialogue.showStr("You earned a " + disguise.name + " " + disguise.type + "!", "happy");
+		} catch (err) {
+			console.error(err);
+		}
+	}
+
+
+
 
 	// PUBLIC
 	return {
 		randomizer: randomizer,
-		returnHtmlStr: returnHtmlStr
+		returnHtmlStr: returnHtmlStr,
+		reward: reward
 	};
 })();
