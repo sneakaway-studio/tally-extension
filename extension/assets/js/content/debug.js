41fa78d9b05861faefe5c5f24314f27c69fc92c4
Owen Mundy <omundy@gmail.com>
2024-03-24 12:43:11 -0400

    Clean up debug

--- extension/assets/js/content/debug.js
+++ extension/assets/js/content/debug.js
@@ -300,15 +300,15 @@ window.Debug = (function() {
 				// Sound.playFile("explosions/explode.mp3", false, 0);
 			});
 
-			Mousetrap.bind(k + ' 0', function() {
-				BattleEffect.showRumble("small");
-			});
-			Mousetrap.bind(k + ' 1', function() {
-				BattleEffect.showRumble("medium");
-			});
-			Mousetrap.bind(k + ' 2', function() {
-				BattleEffect.showRumble("large");
-			});
+			// Mousetrap.bind(k + ' 0', function() {
+			// 	BattleEffect.showRumble("small");
+			// });
+			// Mousetrap.bind(k + ' 1', function() {
+			// 	BattleEffect.showRumble("medium");
+			// });
+			// Mousetrap.bind(k + ' 2', function() {
+			// 	BattleEffect.showRumble("large");
+			// });
 			Mousetrap.bind(k + ' 8', function() {
 				BattleConsole.log("What will Tally do?", "showBattleOptions");
 			});
@@ -320,10 +320,6 @@ window.Debug = (function() {
 			});
 
 
-
-			Mousetrap.bind(k + ' 7', function() {
-
-			});
 
 			Mousetrap.bind(k + ' z', function() {
 				StatsDisplay.adjustStatsBar("tally", "health", Math.random());
